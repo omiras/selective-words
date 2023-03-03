ddd9076fbc1ffe3c424a33e7786f4b41275ca417
omiras <oscar.miras@gmail.com>
2023-03-03 20:32:26 +0000

    errores

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   let words = ["Perro", "Gato", "Loro", "Ardilla", "Orangután", "Ratón"];
   let minSize = 5; // Solo modificar para el ejercicio BONUS
-  let result; // En esta variable debería ir el resultado
+  let result = ""; // En esta variable debería ir el resultado
 
   // Modificar a partir de aquí
 
